3d5fcb4e85ea4fb6e8600a5d18a371ab886d5010
Dave Bushnell <dybushnell@gmail.com>
2019-05-29 01:00:22 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(a){
+
+for (let i = 0; i < 25; i++){
+ if (i == 1){
+ a.push("I am 1 strange loop.")}
+ else{
+   a.push("I am 24 strange loops.")}
+   
+}
+return a 
+}
+
+
+function whileLoop(num){
+  while (num > 0){
+    console.log(--num)
+  }
+var s = "done"
+return s
+}
+
+function doWhileLoop(i){
+  
+}
+
