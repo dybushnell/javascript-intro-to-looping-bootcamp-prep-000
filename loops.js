64551a24a24e29df2577951b48d00b8b5f3ab558
Dave Bushnell <dybushnell@gmail.com>
2019-05-29 01:02:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,7 +20,16 @@ return s
 }
 
 function doWhileLoop(num){
-  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
   
   
 }
